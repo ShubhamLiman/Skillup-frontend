cd3446f49935480b71e9406ff007a84524ca86dc
Shubham Liman <shubhamlstudymail@gmail.com>
2025-01-01 02:03:18 +0530

    --

--- src/components/forms/Form.jsx
+++ src/components/forms/Form.jsx
@@ -24,7 +24,7 @@ function Form({ closeForm }) {
     };
     try {
       const response = await fetch(
-        "https://skillup-backend-shubhamlimans-projects.vercel.app/skillup/api/client/register",
+        "https://skillup-backend-iota.vercel.app/skillup/api/client/register",
         {
           method: "POST",
           headers: {
